bdb6d15a65b91db7932a3725527a82835b1c3e3c
Elexsandro Rangel <elexsandrors@gmail.com>
2021-01-19 22:31:42 -0300

    Apply pending database migrations at startup

--- logviewer-back/LogViewer/LogViewer/Startup.cs
+++ logviewer-back/LogViewer/LogViewer/Startup.cs
@@ -86,6 +86,20 @@ namespace LogViewer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            try 
+            {
+                // migrate any database changes on startup (includes initial db creation)
+                //dataContext.Database.Migrate();
+                using(var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = serviceScope.ServiceProvider.GetService<LogViewerContext>();
+                    context.Database.Migrate();
+                }
+            }
+            finally
+            {
+            }
+
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
